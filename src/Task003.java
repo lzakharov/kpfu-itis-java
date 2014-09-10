676a16606d17d1dcd226bc07fd3b3c11bbdde2b5
Lev Zakharov <lev1996zaharov@gmail.com>
2014-09-10 22:40:36 +0400

    Task003 added

---
+++ src/Task003.java
@@ -0,0 +1,12 @@
+/**
+ * @author Lev Zakharov
+ *         11401
+ *         Created on 10.09.2014.
+ */
+public class Task003 {
+	public static void main(String[] args) {
+		double R = 10.0;
+		double volume = (4.0 * Math.PI * R * R * R) / 3;
+		System.out.println(volume);
+	}	
+}
