3cbf5a23d30e1f16f4782a37a9533eb1b337b8bd
Lev Zakharov <lev1996zaharov@gmail.com>
2014-09-11 20:35:11 +0400

    Task007 added

---
+++ src/Task007.java
@@ -0,0 +1,20 @@
+/**
+ * @author Lev Zakharov
+ *         11401
+ *         Created on 10.09.2014.
+ */
+public class Task007 {
+	public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+		
+		System.out.println("a + b = " + (a + b));
+		System.out.println("a - b = " + (a - b));
+		System.out.println("b - a = " + (b - a));
+		System.out.println("a * b = " + (a * b));
+		System.out.println("a / b = " + (a / b));
+		System.out.println("a % b = " + (a % b));
+		System.out.println("b / a = " + (b / a));
+		System.out.println("b % a = " + (b % a));
+	}	
+}
