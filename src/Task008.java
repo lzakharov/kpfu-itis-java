75e37e9d76ea5a3aefce6415ea1ee077e1e621a4
Lev Zakharov <lev1996zaharov@gmail.com>
2014-09-12 23:43:30 +0400

    Task008 added

--- src/Task008.java
+++ src/Task008.java
@@ -7,14 +7,22 @@ public class Task008 {
 	public static void main(String[] args) {
         byte k = 5;
 		
-		System.out.println("2 * k = " + (2 * k));
-		System.out.println("3 * k = " + (3 * k));
-		System.out.println("4 * k = " + (4 * k));
-		System.out.println("5 * k = " + (5 * k));
-		System.out.println("6 * k = " + (6 * k));
-		System.out.println("7 * k = " + (7 * k));
-		System.out.println("8 * k = " + (8 * k));
-		System.out.println("9 * k = " + (9 * k));
+		//System.out.println("2 * k = " + (2 * k));
+        System.out.println("2 * " + k + " = " + (2 * k));
+		//System.out.println("3 * k = " + (3 * k));
+        System.out.println("3 * " + k + " = " + (3 * k));
+		//System.out.println("4 * k = " + (4 * k));
+        System.out.println("4 * " + k + " = " + (4 * k));
+		//System.out.println("5 * k = " + (5 * k));
+        System.out.println("5 * " + k + " = " + (5 * k));
+		//System.out.println("6 * k = " + (6 * k));
+        System.out.println("6 * " + k + " = " + (6 * k));
+		//System.out.println("7 * k = " + (7 * k));
+        System.out.println("7 * " + k + " = " + (7 * k));
+		//System.out.println("8 * k = " + (8 * k));
+        System.out.println("8 * " + k + " = " + (8 * k));
+		//System.out.println("9 * k = " + (9 * k));
+        System.out.println("9 * " + k + " = " + (9 * k));
 
     }
 }
