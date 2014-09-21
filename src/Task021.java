4a01c2b0cef9c19d3100a1eacf43d4d088c79534
Lev Zakharov <lev1996zaharov@gmail.com>
2014-09-21 11:35:08 +0400

    Tasks 021-022 changed

--- src/Task021.java
+++ src/Task021.java
@@ -11,48 +11,28 @@ public class Task021 {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
 
-        for (int i = 0; i < 2 * n; i++) {
+        for (int i = 0; i < n; i++) {
             for (int j = 0; j < 2 * n - i - 1; j++) {
                 System.out.print(" ");
             }
-            System.out.print("*");
-
-            if (i == n - 1) {
-                for (int j = 0; j < 2 * n - 2; j++) {
-                    System.out.print("*");
-                }
-            } else if (i == 2 * n - 1) {
-                for (int j = 0; j < 4 * n - 2; j++) {
-                    if (j == 2 * n - 2) {
-                        System.out.print(" ");
-                    } else {
-                        System.out.print("*");
-                    }
-                }
-            } else if (i < n - 1) {
-                for (int j = 0; j < 2 * i - 1; j++) {
-                    System.out.print("*");
-                }
-                if (i != 0) {
-                    System.out.print("*");
-                }
-            } else {
-                for (int j = 0; j < 2 * (i - n) - 1; j++) {
-                    System.out.print("*");
-                }
-                if (i != n) {
-                    System.out.print("*");
-                }
-                for (int j = 0; j < 4 * n - 2 * i - 1 ; j++) {
-                    System.out.print(" ");
-                }
+            for (int j = 0; j < 2 * i + 1; j++) {
                 System.out.print("*");
-                for (int j = 0; j < 2 * (i - n) - 1; j++) {
-                    System.out.print("*");
-                }
-                if (i != n) {
-                    System.out.print("*");
-                }
+            }
+            System.out.println();
+        }
+
+        for (int i = n; i < 2 * n; i++) {
+            for (int j = 0; j < 2 * n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * (i - n) + 1; j++) {
+                System.out.print("*");
+            }
+            for (int j = 0; j < 4 * n - 2 * i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * (i - n) + 1; j++) {
+                System.out.print("*");
             }
             System.out.println();
         }
