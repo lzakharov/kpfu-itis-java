997f640a40664a9e45a77d85e1fecbfd8ce3657d
Lev Zakharov <lev1996zaharov@gmail.com>
2014-12-23 12:41:49 +0300

    Task046 updated

--- src/Task046.java
+++ src/Task046.java
@@ -7,16 +7,26 @@
 import java.util.*;
 
 public class Task046 {
+    public static int cmp(String a, String b) {
+        for (int i = 0; i < Math.min(a.length(), b.length()); i++) {
+            if (a.charAt(i) > b.charAt(i)) {
+                return 1;
+            } else if (a.charAt(i) < b.charAt(i)) {
+                return -1;
+            }
+        }
+        return 0;
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String a = in.nextLine();
         String b = in.nextLine();
 
-        if (a.compareTo(b) < 0) {
+        if (cmp(a, b) < 0) {
             System.out.println(a + " < " + b);
-        } else
-        if (a.compareTo(b) == 0) {
-            System.out.println(a + " = " + b);
+        } else if (cmp(a, b) == 0) {
+            System.out.println(a + " == " + b);
         } else {
             System.out.println(a + " > " + b);
         }
