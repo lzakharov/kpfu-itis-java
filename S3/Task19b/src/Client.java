d79188acfbfb6ec6a99b6651bd40e378d5037a38
Lev Zakharov <lev1996zaharov@gmail.com>
2015-11-02 23:50:58 +0300

    Fix Task 19b

--- S3/Task19b/src/Client.java
+++ S3/Task19b/src/Client.java
@@ -1,12 +1,40 @@
 import java.io.*;
 import java.net.Socket;
+import java.util.LinkedList;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by lzakharov on 02.11.15.
  */
 public class Client {
     final static int PORT = 3456;
+
+    private static LinkedList<String> list = new LinkedList<>();
+
+    public static void markCity(String city) {
+        list.add(city);
+    }
+
+    public static boolean isCorrectLettersCase(String city) {
+        Pattern pattern = Pattern.compile("\\p{Upper}(\\p{Lower})+");
+        Matcher matcher = pattern.matcher(city);
+        return matcher.matches();
+    }
+
+    public static boolean isSuitable(String city) {
+        if (list.isEmpty()) {
+            return true;
+        } else {
+            char lastLetter = list.getLast().charAt(list.getLast().length() - 1);
+            return (isCorrectLettersCase(city) &&
+                    Character.isUpperCase(city.charAt(0))
+                    && !list.contains(city)
+                    && lastLetter == Character.toLowerCase(city.charAt(0))
+            );
+        }
+    }
 
     public static void main(String[] args) throws IOException {
         String host = "localhost";
@@ -17,16 +45,16 @@ public class Client {
 
         while (true) {
             String x = in.readLine();
-            Server.markCity(x);
+            markCity(x);
             System.out.println("Server: " + x);
             System.out.println(">");
 
             boolean correctCity = false;
             while (!correctCity) {
                 String y = scanner.nextLine();
-                if (Server.isSuitable(y)) {
+                if (isSuitable(y)) {
                     correctCity = true;
-                    Server.markCity(y);
+                    markCity(y);
                     out.write(y);
                     out.newLine();
                     out.flush();
