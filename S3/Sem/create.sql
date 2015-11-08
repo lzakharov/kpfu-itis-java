83a61ffde51a662a1c10c4d0e5cf71f8dee50139
Lev Zakharov <lev1996zaharov@gmail.com>
2015-11-08 16:28:22 +0300

    Rewrite sql scripts from mysql to postgresql

--- S3/Sem/create.sql
+++ S3/Sem/create.sql
@@ -1,100 +1,107 @@
-CREATE TABLE `USER` (
-  `user_id` INTEGER NOT NULL AUTO_INCREMENT,
-  `username` VARCHAR(20) NOT NULL,
-  `password` VARCHAR(20) NOT NULL,
-  `email` VARCHAR(100) NOT NULL,
-  `birthdate` DATE NOT NULL,
-  `address` VARCHAR(100) NOT NULL,
-  `educational_status` INT(6) NOT NULL,
-  PRIMARY KEY (`user_id`)
+CREATE TABLE "USER" (
+  "user_id" SERIAL,
+  "username" VARCHAR(20) NOT NULL,
+  "first_name" VARCHAR(20) NOT NULL,
+  "last_name" VARCHAR(20) NOT NULL,
+  "password" VARCHAR(20) NOT NULL,
+  "email" VARCHAR(100) NOT NULL,
+  "birthdate" DATE,
+  "address" VARCHAR(100),
+  PRIMARY KEY ("user_id")
 );
 
-CREATE TABLE `MESSAGE` (
-  `message_id` INTEGER NOT NULL AUTO_INCREMENT,
-  `sender_id` INTEGER NOT NULL,
-  `receiver_id` INTEGER NOT NULL,
-  `subject` VARCHAR(100),
-  `text` TEXT NOT NULL,
-  `date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-  PRIMARY KEY (`message_id`)
+CREATE TABLE "ARTICLE" (
+  "article_id" SERIAL,
+  "title" VARCHAR(50) NOT NULL,
+  "author_id" INTEGER NOT NULL,
+  "timestamp" TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  "description" VARCHAR(300),
+  PRIMARY KEY ("article_id")
 );
 
-CREATE TABLE `COMMENT` (
-  `comment_id` INTEGER NOT NULL AUTO_INCREMENT,
-  `news_id` INTEGER NOT NULL,
-  `user_id` INTEGER NOT NULL,
-  `text` TEXT NOT NULL,
-  `date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-  PRIMARY KEY (`comment_id`)
+CREATE TABLE "ARTICLE_DATA" (
+  "article_id" INTEGER NOT NULL,
+  "image" BYTEA,
+  "text" text NOT NULL
 );
 
-CREATE TABLE `NEWS` (
-  `news_id` INTEGER NOT NULL AUTO_INCREMENT,
-  `title` VARCHAR(100) NOT NULL,
-  `text` TEXT NOT NULL,
-  `date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-  PRIMARY KEY (`news_id`)
+CREATE TABLE "AUTHOR" (
+  "author_id" SERIAL,
+  "name" VARCHAR(100) NOT NULL,
+  "birthdate" DATE,
+  "rate" INTEGER,
+  PRIMARY KEY ("author_id")
 );
 
-CREATE TABLE `BOOK` (
-  `book_id` INTEGER NOT NULL AUTO_INCREMENT,
-  `title` VARCHAR(100) NOT NULL,
-  `ISBN` DOUBLE,
-  `rate` INT,
-  PRIMARY KEY (`book_id`)
-);
-		
-CREATE TABLE `REVIEW` (
-  `review_id` INTEGER NOT NULL AUTO_INCREMENT,
-  `book_id` INTEGER NOT NULL,
-  `user_id` INTEGER NOT NULL,
-  `type` INT(2),
-  `text` TEXT NOT NULL,
-  PRIMARY KEY (`review_id`)
-);
-		
-CREATE TABLE `NOTE` (
-  `note_id` INTEGER NOT NULL AUTO_INCREMENT,
-  `title` VARCHAR(100),
-  `text` TEXT,
-  `user_id` INTEGER NOT NULL,
-  `book_id` INTEGER NOT NULL,
-  PRIMARY KEY (`note_id`)
+CREATE TABLE "COMMENT" (
+  "comment_id" SERIAL,
+  "article_id" INTEGER NOT NULL,
+  "author_id" INTEGER NOT NULL,
+  "text" TEXT NOT NULL,
+  "timestamp" TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  PRIMARY KEY ("comment_id")
 );
 
-CREATE TABLE `AUTHOR` (
-  `author_id` INTEGER NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(100) NOT NULL,
-  `birthday` DATE,
-  `rate` INTEGER,
-  PRIMARY KEY (`author_id`)
+CREATE TABLE "BOOK" (
+  "book_id" SERIAL,
+  "name" VARCHAR(100) NOT NULL,
+  "year" INTEGER,
+  "publisher" VARCHAR(100),
+  "rate" INTEGER,
+  PRIMARY KEY ("book_id")
 );
 
-		
-CREATE TABLE `BOOK_AUTHOR` (
-  `id` INTEGER NOT NULL AUTO_INCREMENT,
-  `book_id` INTEGER NOT NULL,
-  `author_id` INTEGER NOT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE "BOOK_AUTHOR" (
+  "book_id" INTEGER NOT NULL,
+  "author_id" INTEGER NOT NULL
 );
 
-CREATE TABLE `BOOK_DATA` (
-  `id` INTEGER NOT NULL AUTO_INCREMENT,
-  `book_id` INTEGER NOT NULL,
-  `description` TEXT,
-  `text` BLOB,
-  `cover` BLOB,
-  PRIMARY KEY (`id`)
+CREATE TABLE "BOOK_DATA" (
+  "book_id" INTEGER NOT NULL,
+  "description" TEXT,
+  "text" BYTEA,
+  "cover" BYTEA
 );
 
-ALTER TABLE `MESSAGE` ADD FOREIGN KEY (sender_id) REFERENCES `USER` (`user_id`);
-ALTER TABLE `MESSAGE` ADD FOREIGN KEY (receiver_id) REFERENCES `USER` (`user_id`);
-ALTER TABLE `COMMENT` ADD FOREIGN KEY (news_id) REFERENCES `NEWS` (`news_id`);
-ALTER TABLE `COMMENT` ADD FOREIGN KEY (user_id) REFERENCES `USER` (`user_id`);
-ALTER TABLE `REVIEW` ADD FOREIGN KEY (book_id) REFERENCES `BOOK` (`book_id`);
-ALTER TABLE `REVIEW` ADD FOREIGN KEY (user_id) REFERENCES `USER` (`user_id`);
-ALTER TABLE `NOTE` ADD FOREIGN KEY (user_id) REFERENCES `USER` (`user_id`);
-ALTER TABLE `NOTE` ADD FOREIGN KEY (book_id) REFERENCES `BOOK` (`book_id`);
-ALTER TABLE `BOOK_AUTHOR` ADD FOREIGN KEY (book_id) REFERENCES `BOOK` (`book_id`);
-ALTER TABLE `BOOK_AUTHOR` ADD FOREIGN KEY (author_id) REFERENCES `AUTHOR` (`author_id`);
-ALTER TABLE `BOOK_DATA` ADD FOREIGN KEY (book_id) REFERENCES `BOOK` (`book_id`);
+CREATE TABLE "MESSAGE" (
+  "message_id" SERIAL,
+  "sender_id" INTEGER NOT NULL,
+  "receiver_id" INTEGER NOT NULL,
+  "subject" VARCHAR(100),
+  "text" TEXT NOT NULL,
+  "date" TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  PRIMARY KEY ("message_id")
+);
+
+CREATE TABLE "NOTE" (
+  "note_id" SERIAL,
+  "user_id" INTEGER NOT NULL,
+  "book_id" INTEGER NOT NULL,
+  "title" VARCHAR(100),
+  "text" TEXT,
+  PRIMARY KEY ("note_id")
+);
+
+CREATE TABLE "REVIEW" (
+  "review_id" SERIAL,
+  "book_id" INTEGER NOT NULL,
+  "user_id" INTEGER NOT NULL,
+  "type" INTEGER NOT NULL,
+  "text" TEXT NOT NULL,
+  "timestamp" TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  PRIMARY KEY ("review_id")
+);
+
+ALTER TABLE "ARTICLE" ADD FOREIGN KEY ("author_id") REFERENCES "USER" ("user_id");
+ALTER TABLE "ARTICLE_DATA" ADD FOREIGN KEY ("article_id") REFERENCES "ARTICLE" ("article_id");
+ALTER TABLE "COMMENT" ADD FOREIGN KEY ("article_id") REFERENCES "ARTICLE" ("article_id");
+ALTER TABLE "COMMENT" ADD FOREIGN KEY ("author_id") REFERENCES "USER" ("user_id");
+ALTER TABLE "BOOK_DATA" ADD FOREIGN KEY ("book_id") REFERENCES "BOOK" ("book_id");
+ALTER TABLE "BOOK_AUTHOR" ADD FOREIGN KEY ("book_id") REFERENCES "BOOK" ("book_id");
+ALTER TABLE "BOOK_AUTHOR" ADD FOREIGN KEY ("author_id") REFERENCES "AUTHOR" ("author_id");
+ALTER TABLE "MESSAGE" ADD FOREIGN KEY ("sender_id") REFERENCES "USER" ("user_id");
+ALTER TABLE "MESSAGE" ADD FOREIGN KEY ("receiver_id") REFERENCES "USER" ("user_id");
+ALTER TABLE "NOTE" ADD FOREIGN KEY ("user_id") REFERENCES "USER" ("user_id");
+ALTER TABLE "NOTE" ADD FOREIGN KEY ("book_id") REFERENCES "BOOK" ("book_id");
+ALTER TABLE "REVIEW" ADD FOREIGN KEY ("book_id") REFERENCES "BOOK" ("book_id");
+ALTER TABLE "REVIEW" ADD FOREIGN KEY ("user_id") REFERENCES "USER" ("user_id");
--- S3/Sem/data.sql
+++ S3/Sem/data.sql
@@ -1,45 +1,38 @@
-INSERT INTO `USER` (`username`, `password`, `email`, `birthdate`, `address`, `educational_status`) VALUES('user1', '12345', 'user1@mail.com', '1900-1-1', 'address1', '1');
-INSERT INTO `USER` (`username`, `password`, `email`, `birthdate`, `address`, `educational_status`) VALUES('user2', '12345', 'user2@mail.com', '1980-2-20', 'address2', '2');
-INSERT INTO `USER` (`username`, `password`, `email`, `birthdate`, `address`, `educational_status`) VALUES('user3', '12345', 'user3@mail.com', '2000-1-1', 'address3', '1');
+INSERT INTO "USER" ("user_id", "username", "first_name", "last_name", "password", "email") VALUES(1, 'lev', 'Lev', 'Zakharov', 'Ololo111', 'lev@example.coml');
+INSERT INTO "USER" ("user_id", "username", "first_name", "last_name", "password", "email") VALUES(2, 'ivan', 'Ivan', 'Ivanov', 'Qwerty123', 'ivan@example.coml');
+INSERT INTO "USER" ("user_id", "username", "first_name", "last_name", "password", "email") VALUES(3, 'john', 'John', 'Doe', 'Asdfg123', 'john@example.coml');
 
-INSERT INTO `MESSAGE` (`sender_id`, `receiver_id`, `subject`, `text`) VALUES('1', '2', 'subject1', 'Hi!');
-INSERT INTO `MESSAGE` (`sender_id`, `receiver_id`, `subject`, `text`) VALUES('2', '1', 'subject1', 'Hi! How r u?');
-INSERT INTO `MESSAGE` (`sender_id`, `receiver_id`, `subject`, `text`) VALUES('3', '1', 'subject1', 'Hello!:)');
+INSERT INTO "ARTICLE" VALUES(DEFAULT, 'Title', 1, CURRENT_TIMESTAMP, 'This is description of first article');
+INSERT INTO "ARTICLE" VALUES(DEFAULT, 'Title', 2, CURRENT_TIMESTAMP, 'This is description of second article');
+INSERT INTO "ARTICLE" VALUES(DEFAULT, 'Title', 1, CURRENT_TIMESTAMP, 'This is description');
 
-INSERT INTO `NEWS` (`title`, `text`) VALUES('Very interesting news', 'Very interesting text:)');
-INSERT INTO `NEWS` (`title`, `text`) VALUES('Very boring news', 'Very boring text:/');
-INSERT INTO `NEWS` (`title`, `text`) VALUES('Normal news', 'Here some text...');
+INSERT INTO "ARTICLE_DATA" VALUES(1, NULL, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi a erat ac libero feugiat fringilla. Donec ullamcorper sodales justo sit amet molestie. Fusce commodo velit ut mauris finibus, vel condimentum lorem molestie. Nam et ullamcorper justo. Morbi tempus, risus quis dapibus congue, nisi mi dictum nibh, vel dictum odio magna non enim. Aliquam hendrerit justo in turpis dictum venenatis. Vivamus auctor purus enim, at tincidunt nisi pulvinar et.
+Cras id libero libero. Cras imperdiet volutpat eros sit amet scelerisque. Nulla sit amet ex mi. Vestibulum nisi metus, dapibus nec diam luctus, placerat blandit nunc. Etiam scelerisque maximus egestas. Quisque diam ex, condimentum nec ligula at, maximus fermentum nisl. Maecenas urna ipsum, ornare a bibendum consequat, bibendum a nisl. Nullam quis sapien condimentum, rutrum ex vel, mattis sapien. Donec gravida augue molestie, bibendum nisi sit amet, convallis ante.');
+INSERT INTO "ARTICLE_DATA" VALUES(2, NULL, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi a erat ac libero feugiat fringilla. Donec ullamcorper sodales justo sit amet molestie. Fusce commodo velit ut mauris finibus, vel condimentum lorem molestie. Nam et ullamcorper justo. Morbi tempus, risus quis dapibus congue, nisi mi dictum nibh, vel dictum odio magna non enim. Aliquam hendrerit justo in turpis dictum venenatis. Vivamus auctor purus enim, at tincidunt nisi pulvinar et.
+Cras id libero libero. Cras imperdiet volutpat eros sit amet scelerisque. Nulla sit amet ex mi. Vestibulum nisi metus, dapibus nec diam luctus, placerat blandit nunc. Etiam scelerisque maximus egestas. Quisque diam ex, condimentum nec ligula at, maximus fermentum nisl. Maecenas urna ipsum, ornare a bibendum consequat, bibendum a nisl. Nullam quis sapien condimentum, rutrum ex vel, mattis sapien. Donec gravida augue molestie, bibendum nisi sit amet, convallis ante.');
+INSERT INTO "ARTICLE_DATA" VALUES(3, NULL, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi a erat ac libero feugiat fringilla. Donec ullamcorper sodales justo sit amet molestie. Fusce commodo velit ut mauris finibus, vel condimentum lorem molestie. Nam et ullamcorper justo. Morbi tempus, risus quis dapibus congue, nisi mi dictum nibh, vel dictum odio magna non enim. Aliquam hendrerit justo in turpis dictum venenatis. Vivamus auctor purus enim, at tincidunt nisi pulvinar et.
+Cras id libero libero. Cras imperdiet volutpat eros sit amet scelerisque. Nulla sit amet ex mi. Vestibulum nisi metus, dapibus nec diam luctus, placerat blandit nunc. Etiam scelerisque maximus egestas. Quisque diam ex, condimentum nec ligula at, maximus fermentum nisl. Maecenas urna ipsum, ornare a bibendum consequat, bibendum a nisl. Nullam quis sapien condimentum, rutrum ex vel, mattis sapien. Donec gravida augue molestie, bibendum nisi sit amet, convallis ante.');
 
-INSERT INTO `COMMENT` (`news_id`, `user_id`, `text`) VALUES('1', '1', 'WOW! Such interesting!');
-INSERT INTO `COMMENT` (`news_id`, `user_id`, `text`) VALUES('2', '2', 'Damn! So boring -...-');
-INSERT INTO `COMMENT` (`news_id`, `user_id`, `text`) VALUES('1', '1', 'Ok, nice text.');
+INSERT INTO "COMMENT" VALUES(DEFAULT, 1, 1, 'This is my first comment', DEFAULT);
 
-INSERT INTO `COMMENT` (`news_id`, `user_id`, `text`) VALUES('1', '1', 'Ok, nice text.');
-INSERT INTO `COMMENT` (`news_id`, `user_id`, `text`) VALUES('1', '1', 'Ok, nice text.');
-INSERT INTO `COMMENT` (`news_id`, `user_id`, `text`) VALUES('1', '1', 'Ok, nice text.');
+INSERT INTO "BOOK" (name) VALUES('Harry Potter');
+INSERT INTO "BOOK" (name) VALUES('The Call of Cthulhu');
+INSERT INTO "BOOK" (name) VALUES('Surely You''re Joking, Mr. Feynman!');
 
-INSERT INTO `BOOK` (`title`) VALUES('Harry Potter');
-INSERT INTO `BOOK` (`title`) VALUES('The Call of Cthulhu');
-INSERT INTO `BOOK` (`title`) VALUES('Surely You\'re Joking, Mr. Feynman!');
+INSERT INTO "AUTHOR" ("name", "rate") VALUES('J.K. Rowling', 4);
+INSERT INTO "AUTHOR" ("name", "birthdate", "rate") VALUES('Howard Phillips Lovecraft', '1890-8-20', 5);
+INSERT INTO "AUTHOR" ("name") VALUES('Ralph Leighton');
+INSERT INTO "AUTHOR" ("name", "birthdate", "rate") VALUES('Richard Phillips Feynman', '1918-5-11', 5);
 
-INSERT INTO `REVIEW` (`book_id`, `user_id`, `type`, `text`) VALUES('1', '1', '1', 'Such magic!');
-INSERT INTO `REVIEW` (`book_id`, `user_id`, `type`, `text`) VALUES('2', '1', '1', 'Ph\' nglui mglw\'nafh Cthulhu R\'lyeh wgah\'nagl fhtagn');
-INSERT INTO `REVIEW` (`book_id`, `user_id`, `type`, `text`) VALUES('3', '3', '1', 'My favorite book!:)');
+INSERT INTO "BOOK_AUTHOR" ("book_id", "author_id") VALUES(1, 1);
+INSERT INTO "BOOK_AUTHOR" ("book_id", "author_id") VALUES(2, 2);
+INSERT INTO "BOOK_AUTHOR" ("book_id", "author_id") VALUES(3, 3);
+INSERT INTO "BOOK_AUTHOR" ("book_id", "author_id") VALUES(3, 4);
 
-INSERT INTO `NOTE` (`title`, `text`, `user_id`, `book_id`) VALUES('note1', 'This my first note', '1', '1');
-INSERT INTO `NOTE` (`title`, `text`, `user_id`, `book_id`) VALUES('note2', 'This my second note', '1', '1');
-INSERT INTO `NOTE` (`title`, `text`, `user_id`, `book_id`) VALUES('note1', 'This my first note', '2', '1');
+INSERT INTO "BOOK_DATA" ("book_id", "description") VALUES(1, 'Harry Potter is an English series of seven fantasy novels written by British author J. K. Rowling. The series chronicles the various adventures of a young wizard, Harry Potter, the titular character, and his friends Ronald Weasley and Hermione Granger, all of whom are students at Hogwarts School of Witchcraft and Wizardry. The main story arc concerns Harr''s quest to defeat the Dark wizard Lord Voldemort, who aims to become immortal, conquer the wizarding world, subjugate non-magical people, and destroy all those who stand in his way, especially Harry Potter.');
+INSERT INTO "BOOK_DATA" ("book_id", "description") VALUES(2, '"The Call of Cthulhu" is a short story by American writer H. P. Lovecraft. Written in the summer of 1926, it was first published in the pulp magazine Weird Tales, in February 1928.');
+INSERT INTO "BOOK_DATA" ("book_id", "description") VALUES(3, '"Surely You''re Joking, Mr. Feynman!": Adventures of a Curious Character is an edited collection of reminiscences by the Nobel Prize-winning physicist Richard Feynman. The book, released in 1985, covers a variety of instances in Feynman''s life. Some are lighthearted in tone, such as his fascination with safe-cracking, studying various languages, participating with groups of people who share different interests (such as biology or philosophy), and ventures into art and samba music. Others cover more serious material, including his work on the Manhattan Project (during which his first wife Arline Greenbaum died of tuberculosis) and his critique of the science education system in Brazil. The section "Monster Minds" describes his slightly nervous presentation of his graduate work on the Wheeler–Feynman absorber theory in front of Albert Einstein, Wolfgang Pauli and other major figures of the time.');
 
-INSERT INTO `AUTHOR` (`name`, `rate`) VALUES('J.K. Rowling', 4);
-INSERT INTO `AUTHOR` (`name`, `birthdate`, `rate`) VALUES('Howard Phillips Lovecraft', '1890-8-20', 5);
-INSERT INTO `AUTHOR` (`name`) VALUES('Ralph Leighton');
-INSERT INTO `AUTHOR` (`name`, `birthdate`, `rate`) VALUES('Richard Phillips Feynman', '1918-5-11', 5);
-
-INSERT INTO `BOOK_AUTHOR` (`book_id`, `author_id`) VALUES(1, 1);
-INSERT INTO `BOOK_AUTHOR` (`book_id`, `author_id`) VALUES(2, 2);
-INSERT INTO `BOOK_AUTHOR` (`book_id`, `author_id`) VALUES(3, 3);
-INSERT INTO `BOOK_AUTHOR` (`book_id`, `author_id`) VALUES(3, 4);
-
-INSERT INTO `BOOK_DATA` (`book_id`, `description`) VALUES(1, 'Harry Potter is an English series of seven fantasy novels written by British author J. K. Rowling. The series chronicles the various adventures of a young wizard, Harry Potter, the titular character, and his friends Ronald Weasley and Hermione Granger, all of whom are students at Hogwarts School of Witchcraft and Wizardry. The main story arc concerns Harry\'s quest to defeat the Dark wizard Lord Voldemort, who aims to become immortal, conquer the wizarding world, subjugate non-magical people, and destroy all those who stand in his way, especially Harry Potter.');
-INSERT INTO `BOOK_DATA` (`book_id`, `description`) VALUES(2, '\"The Call of Cthulhu\" is a short story by American writer H. P. Lovecraft. Written in the summer of 1926, it was first published in the pulp magazine Weird Tales, in February 1928.');
-INSERT INTO `BOOK_DATA` (`book_id`, `description`) VALUES(3, '\"Surely You\'re Joking, Mr. Feynman!\": Adventures of a Curious Character is an edited collection of reminiscences by the Nobel Prize-winning physicist Richard Feynman. The book, released in 1985, covers a variety of instances in Feynman\'s life. Some are lighthearted in tone, such as his fascination with safe-cracking, studying various languages, participating with groups of people who share different interests (such as biology or philosophy), and ventures into art and samba music. Others cover more serious material, including his work on the Manhattan Project (during which his first wife Arline Greenbaum died of tuberculosis) and his critique of the science education system in Brazil. The section \"Monster Minds\" describes his slightly nervous presentation of his graduate work on the Wheeler–Feynman absorber theory in front of Albert Einstein, Wolfgang Pauli and other major figures of the time.');
+INSERT INTO "REVIEW" ("book_id", "user_id", "type", "text", "timestamp") VALUES('1', '1', '1', 'Such magic!', CURRENT_TIMESTAMP);
+INSERT INTO "REVIEW" ("book_id", "user_id", "type", "text", "timestamp") VALUES('2', '1', '1', 'Ph'' nglui mglw''nafh Cthulhu R''lyeh wgah''nagl fhtagn', CURRENT_TIMESTAMP);
+INSERT INTO "REVIEW" ("book_id", "user_id", "type", "text", "timestamp") VALUES('3', '1', '1', 'My favorite book!:)', CURRENT_TIMESTAMP);
